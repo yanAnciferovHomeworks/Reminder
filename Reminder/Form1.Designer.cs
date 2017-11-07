37bc9c0f95458778a8e67d1c767c22cf3a05afdf
Анциферов Ян <Anci_oq88>
2017-11-07 17:53:40 +0200

    second commit

--- Reminder/Form1.Designer.cs
+++ Reminder/Form1.Designer.cs
@@ -28,39 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.Calendar = new System.Windows.Forms.MonthCalendar();
             this.EventBox = new System.Windows.Forms.GroupBox();
-            this.NameEvent = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.DateEvent = new System.Windows.Forms.DateTimePicker();
-            this.label2 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.CencelButton = new System.Windows.Forms.Button();
+            this.OKButton = new System.Windows.Forms.Button();
+            this.IsRemind = new System.Windows.Forms.CheckBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.Repeat = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.DateEvent = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.NameEvent = new System.Windows.Forms.TextBox();
             this.Events = new System.Windows.Forms.ListBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.AddButton = new System.Windows.Forms.Button();
-            this.EditButton = new System.Windows.Forms.Button();
             this.DelButton = new System.Windows.Forms.Button();
+            this.EditButton = new System.Windows.Forms.Button();
+            this.AddButton = new System.Windows.Forms.Button();
             this.EventBox.SuspendLayout();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // monthCalendar1
+            // Calendar
             // 
-            this.monthCalendar1.Location = new System.Drawing.Point(13, 47);
-            this.monthCalendar1.Name = "monthCalendar1";
-            this.monthCalendar1.TabIndex = 0;
+            this.Calendar.Location = new System.Drawing.Point(13, 47);
+            this.Calendar.Name = "Calendar";
+            this.Calendar.TabIndex = 0;
             // 
             // EventBox
             // 
-            this.EventBox.Controls.Add(this.button2);
-            this.EventBox.Controls.Add(this.button1);
-            this.EventBox.Controls.Add(this.checkBox1);
+            this.EventBox.Controls.Add(this.CencelButton);
+            this.EventBox.Controls.Add(this.OKButton);
+            this.EventBox.Controls.Add(this.IsRemind);
             this.EventBox.Controls.Add(this.label3);
-            this.EventBox.Controls.Add(this.comboBox1);
+            this.EventBox.Controls.Add(this.Repeat);
             this.EventBox.Controls.Add(this.label2);
             this.EventBox.Controls.Add(this.DateEvent);
             this.EventBox.Controls.Add(this.label1);
@@ -72,21 +72,66 @@
             this.EventBox.TabStop = false;
             this.EventBox.Text = "Событие";
             // 
-            // NameEvent
+            // CencelButton
             // 
-            this.NameEvent.Location = new System.Drawing.Point(72, 19);
-            this.NameEvent.Name = "NameEvent";
-            this.NameEvent.Size = new System.Drawing.Size(265, 20);
-            this.NameEvent.TabIndex = 0;
+            this.CencelButton.Location = new System.Drawing.Point(225, 132);
+            this.CencelButton.Name = "CencelButton";
+            this.CencelButton.Size = new System.Drawing.Size(75, 23);
+            this.CencelButton.TabIndex = 8;
+            this.CencelButton.Text = "Cancel";
+            this.CencelButton.UseVisualStyleBackColor = true;
+            this.CencelButton.Click += new System.EventHandler(this.button2_Click);
             // 
-            // label1
+            // OKButton
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(7, 25);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(57, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Название";
+            this.OKButton.Location = new System.Drawing.Point(144, 132);
+            this.OKButton.Name = "OKButton";
+            this.OKButton.Size = new System.Drawing.Size(75, 23);
+            this.OKButton.TabIndex = 7;
+            this.OKButton.Text = "OK";
+            this.OKButton.UseVisualStyleBackColor = true;
+            this.OKButton.Click += new System.EventHandler(this.OKButton_Click);
+            // 
+            // IsRemind
+            // 
+            this.IsRemind.AutoSize = true;
+            this.IsRemind.Location = new System.Drawing.Point(6, 138);
+            this.IsRemind.Name = "IsRemind";
+            this.IsRemind.Size = new System.Drawing.Size(89, 17);
+            this.IsRemind.TabIndex = 6;
+            this.IsRemind.Text = "Напоминать";
+            this.IsRemind.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Повторять";
+            // 
+            // Repeat
+            // 
+            this.Repeat.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Repeat.FormattingEnabled = true;
+            this.Repeat.Items.AddRange(new object[] {
+            "Один раз",
+            "Каждый месяц",
+            "Каждый год"});
+            this.Repeat.Location = new System.Drawing.Point(72, 95);
+            this.Repeat.Name = "Repeat";
+            this.Repeat.Size = new System.Drawing.Size(265, 21);
+            this.Repeat.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Время";
             // 
             // DateEvent
             // 
@@ -100,59 +145,21 @@
             this.DateEvent.Size = new System.Drawing.Size(265, 20);
             this.DateEvent.TabIndex = 2;
             // 
-            // label2
+            // label1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(7, 63);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(40, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Время";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Название";
             // 
-            // comboBox1
+            // NameEvent
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(72, 95);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(265, 21);
-            this.comboBox1.TabIndex = 4;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(7, 103);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(61, 13);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Повторять";
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(6, 138);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(89, 17);
-            this.checkBox1.TabIndex = 6;
-            this.checkBox1.Text = "Напоминать";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(144, 132);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "OK";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(225, 132);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 8;
-            this.button2.Text = "Cancel";
-            this.button2.UseVisualStyleBackColor = true;
+            this.NameEvent.Location = new System.Drawing.Point(72, 19);
+            this.NameEvent.Name = "NameEvent";
+            this.NameEvent.Size = new System.Drawing.Size(265, 20);
+            this.NameEvent.TabIndex = 0;
             // 
             // Events
             // 
@@ -172,6 +179,27 @@
             this.panel1.Size = new System.Drawing.Size(449, 29);
             this.panel1.TabIndex = 3;
             // 
+            // DelButton
+            // 
+            this.DelButton.Enabled = false;
+            this.DelButton.Location = new System.Drawing.Point(190, 3);
+            this.DelButton.Name = "DelButton";
+            this.DelButton.Size = new System.Drawing.Size(100, 23);
+            this.DelButton.TabIndex = 2;
+            this.DelButton.Text = "Удалить";
+            this.DelButton.UseVisualStyleBackColor = true;
+            // 
+            // EditButton
+            // 
+            this.EditButton.Enabled = false;
+            this.EditButton.Location = new System.Drawing.Point(84, 3);
+            this.EditButton.Name = "EditButton";
+            this.EditButton.Size = new System.Drawing.Size(100, 23);
+            this.EditButton.TabIndex = 1;
+            this.EditButton.Text = "Редактировать";
+            this.EditButton.UseVisualStyleBackColor = true;
+            this.EditButton.Click += new System.EventHandler(this.EditButton_Click);
+            // 
             // AddButton
             // 
             this.AddButton.Location = new System.Drawing.Point(5, 3);
@@ -180,33 +208,16 @@
             this.AddButton.TabIndex = 0;
             this.AddButton.Text = "Добавить";
             this.AddButton.UseVisualStyleBackColor = true;
-            // 
-            // EditButton
-            // 
-            this.EditButton.Location = new System.Drawing.Point(84, 3);
-            this.EditButton.Name = "EditButton";
-            this.EditButton.Size = new System.Drawing.Size(100, 23);
-            this.EditButton.TabIndex = 1;
-            this.EditButton.Text = "Редактировать";
-            this.EditButton.UseVisualStyleBackColor = true;
-            // 
-            // DelButton
-            // 
-            this.DelButton.Location = new System.Drawing.Point(190, 3);
-            this.DelButton.Name = "DelButton";
-            this.DelButton.Size = new System.Drawing.Size(100, 23);
-            this.DelButton.TabIndex = 2;
-            this.DelButton.Text = "Удалить";
-            this.DelButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
             // 
             // RemindForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(375, 222);
+            this.ClientSize = new System.Drawing.Size(375, 397);
             this.Controls.Add(this.Events);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.Calendar);
             this.Controls.Add(this.EventBox);
             this.Name = "RemindForm";
             this.Text = "Напоминальник";
@@ -219,16 +230,16 @@
 
         #endregion
 
-        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.MonthCalendar Calendar;
         private System.Windows.Forms.GroupBox EventBox;
         private System.Windows.Forms.DateTimePicker DateEvent;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox NameEvent;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button CencelButton;
+        private System.Windows.Forms.Button OKButton;
+        private System.Windows.Forms.CheckBox IsRemind;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox Repeat;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ListBox Events;
         private System.Windows.Forms.Panel panel1;
--- Reminder/Form1.cs
+++ Reminder/Form1.cs
@@ -15,6 +15,71 @@ namespace Reminder
         public RemindForm()
         {
             InitializeComponent();
+            ResizeWinToSmall();
         }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            DateEvent.Value = new DateTime().ToLocalTime();
+            AddButton.Enabled = false;
+            ResizeWinToBig();
+        }
+
+        private void ResizeWinToBig()
+        {
+            Size = new Size(Size.Width, Size.Height + 180);
+        }
+
+        private void ResizeWinToSmall()
+        {
+            Size = new Size(Size.Width, Size.Height - 180);
+        }
+
+        private void EditButton_Click(object sender, EventArgs e)
+        {
+            ResizeWinToBig();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ResizeWinToSmall();
+            AddButton.Enabled = true;
+        }
+
+        private void OKButton_Click(object sender, EventArgs e)
+        {
+            Event ev = new Event();
+            ev.Name = NameEvent.Text;
+            if (ev.Name == "")
+                return;
+
+            ev.IsRemind = IsRemind.Checked;
+
+            ev.GetRemind = (RemindMode)Repeat.SelectedValue;
+
+            ev.Time = DateEvent.Value;
+            
+
+        }
+
+        
+    }
+
+    public class Event
+    {
+        public string Name { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public bool IsRemind { get; set; }
+
+        public RemindMode GetRemind { get; set; }
+    }
+
+    public enum RemindMode
+    {
+        One = 0,
+        EveryMonth,
+        EveryYear
     }
 }
